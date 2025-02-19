2c5f6ae7341545f027caceb62be4a886a4e73eaf
GENTLEW1ND <rajchakraborty029@gmail.com>
2025-02-19 19:51:05 +0530

    Data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from yt_mlProject import logger # type: ignore
+from yt_mlProject import logger
+from yt_mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is second test log message.")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=============x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/yt_mlProject/config/configuration.py
@@ -0,0 +1,30 @@
+from yt_mlProject.constants import *
+from yt_mlProject.utils.common import read_yaml, create_directories
+from yt_mlProject.entity.config_entity import DataIngestionConfig 
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH
+    ):
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+        
+        create_directories([self.config.artifacts_root])
+        
+        
+    def get_data_ingestion_config(self)-> DataIngestionConfig:
+        config = self.config.data_ingesting
+        
+        create_directories([config.root_dir])
+        
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file
+        )
+        
+        return data_ingestion_config
