6cd1722fdb2045bb00c878da6af5061db957a8c8
GENTLEW1ND <rajchakraborty029@gmail.com>
2025-02-21 10:24:59 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from yt_mlProject import logger
 from yt_mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from yt_mlProject.pipeline.stage_02_data_validaiton  import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=============x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        data_validation = DataValidationTrainingPipeline()
+        data_validation.main()
         logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=============x")
 except Exception as e:
         logger.exception(e)
